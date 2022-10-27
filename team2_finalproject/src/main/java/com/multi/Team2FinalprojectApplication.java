3814e4f0c5db75f23f8296586e8284ced8d243e9
jaeyooon <jycho0208@gmail.com>
2022-10-27 09:47:13 +0900

    first commit

---
+++ team2_finalproject/src/main/java/com/multi/Team2FinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2FinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2FinalprojectApplication.class, args);
+	}
+
+}
