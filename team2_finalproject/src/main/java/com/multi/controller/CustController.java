21f03cd5b1926b57927948e5b15660e40a0f95a8
ChoJae-won <jwcho1996@gmail.com>
2022-11-01 15:08:41 +0900

    register 삭제

--- team2_finalproject/src/main/java/com/multi/controller/CustController.java
+++ team2_finalproject/src/main/java/com/multi/controller/CustController.java
@@ -24,38 +24,6 @@ public class CustController {
 		model.addAttribute("center", dir+"login");
 		return "main";
 	}
-	
-	@RequestMapping("/register")
-	public String register(Model model) {
-		model.addAttribute("center", dir+"register");
-		return "main";
-	}
 
 
-@RequestMapping("/loginimpl")
-public String loginimpl(String custid, String pwd, Model model, HttpSession session) {
-	CustomerDTO cust = null;
-	try {
-		cust = cust_service.get(custid);
-		if (cust == null) {
-			model.addAttribute("center", dir + "loginfail");
-		} else {
-			if (cust.getWithdrawal() == 1) {
-				if (pwd.equals(cust.getPwd())) {
-					session.setAttribute("logincust", cust);
-					model.addAttribute("center", "main");
-				} else {
-					model.addAttribute("center", dir + "loginfail");
-				}
-				// id,pwd가 다 아닐 때
-			} else {
-				model.addAttribute("center", dir + "loginfail");
-			}
-		}
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	return "redirect: main";
-}
 }
